f6a690d1920e5136cfaaa426c8da846442bda578
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-18 14:48:33 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,35 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static String pluralize(String word){
+		if(word.toLowerCase().endsWith("ey")){
+			return "eys";
+		}else if(word.toLowerCase().endsWith("y")){
+			return "ies";
+		}else if(word.toLowerCase().endsWith("ife")){
+			return "ives";
+		}else{
+			return "s";
+		}
+	}
 
+	public static int min(int num1, int num2, int num3){
+		return Math.min(Math.min(num1, num2), num3);
+	}
+
+	public static boolean isLeapYear(int year){
+		if(year % 400 == 0){
+			return true;
+		}else if(year % 100 == 0){
+			return false;
+		}else if(year % 4 == 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public static void main(String args[]) {
+		
 	}
 
 }
